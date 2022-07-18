44528c8187c11a538b566de8843a97baaaec1289
周宇杰 <1348562017@qq.com>
2022-07-18 16:31:58 +0800

    更新0.1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>vue3 生命周期</h1>
+    <div id="dom">{{msg}}--{{num}}</div>
   </div>
 </template>
+<script>
+import { reactive, toRefs, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue'
+export default {
+  name: 'about',
+  setup () {
+    const data = reactive({
+      msg: 'hello',
+      num: 1
+    })
+    // 数据渲染前
+    onBeforeMount(() => {
+      console.log('onBeforeMount', document.querySelector('#dom'))
+    })
+    // 数据渲染后
+    onMounted(() => {
+      console.log('onMount', document.querySelector('#dom'))
+      setTimeout(() => {
+        data.msg = 'world'
+      }, 2000)
+    })
+    // dom更新前
+    onBeforeUpdate(() => {
+      console.log('onBeforUpdate')
+    })
+    // dom更新后
+    onUpdated(() => {
+      console.log('onUpdated')
+      // data.num++ 触发死循环
+    })
+    return {
+      ...toRefs(data)
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>hello world!</h1>
+    <p v-text="name"></p>
+    <!-- v-text 简写 -->
+    <p>{{age}}</p>
+    <!-- v-html 网页标签 -->
+    <p v-html="info"></p>
+    <!-- v-bind:属性名:"变量名" 绑定动态的标签属性 简写： :属性名 -->
+    <p v-bind:data="dataVal">我有属性data</p>
+    <!-- class类名绑定 可以叠加使用 -->
+    <p class="text" :class="{'red':isRed}">我是红色的</p>
+    <!-- v-if 判断语句-->
+    <!-- v-show false的时候是样式上的隐藏 -->
+    <p v-if="!isTrue">v-if</p>
+    <p v-show="isTrue">v-show</p>
+     <!-- v-if v-else-->
+    <p v-if="!isTrue">if</p>
+    <p v-else>else</p>
+    <!-- for 循环 -->
+    <!-- 循环userList -->
+    <!-- 语法：v-for="(每一个对象的变量,下标) in 数组" -->
+    <ul>
+      <li v-for="(value,index) in userList" :key="index">
+        学生姓名：{{ value.username}}
+        学生年龄：{{ value.userage}}
+      </li>
+    </ul>
+ <p></p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { reactive, toRefs } from 'vue'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'home',
+  setup () {
+  // beforCreate和created这两个生命周期
+    const data = reactive({
+      name: '小红',
+      age: 20,
+      info: '<i>我是斜体字</i>',
+      dataVal: 20,
+      isRed: true,
+      isTrue: true,
+      isFalse: false,
+      userList: [
+        {
+          username: '胡',
+          userage: 20
+        },
+        {
+          username: '红',
+          userage: 18
+        }
+      ]
+    })
+    return {
+      ...toRefs(data)
+    }
   }
 }
 </script>
+<style>
+.red{
+  color: red;
+}
+</style>
